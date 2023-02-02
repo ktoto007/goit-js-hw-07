d1ff12a819ba29ce2a1302e55a850cdd2ad3dd89
Streltsov Valerii <st.valera04@gmail.com>
2023-02-02 23:20:11 +0200

    Done first exersise

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,64 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItemCollection = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+    <div class="gallery__item">
+      <a class="gallery__link" href="large-image.jpg">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div> `
+  )
+  .join("");
+
+gallery.innerHTML = galleryItemCollection;
+
+function openBigImg(evt) {
+  evt.preventDefault();
+
+  const element = evt.target;
+
+  if (!element.classList.contains("gallery__image")) {
+    return;
+  }
+
+  createModalWindow(takeUrlBigImg(element));
+}
+
+function takeUrlBigImg(elem) {
+  const urlBigImg = elem.dataset.source;
+  return urlBigImg;
+}
+
+let instance;
+
+function createModalWindow(result) {
+  instance = basicLightbox.create(`
+        <img src="${result}">
+    `);
+
+  instance.show();
+}
+
+gallery.addEventListener("click", openBigImg);
+
+function closeBigImg(evt) {
+  if (!instance.visible) {
+    return;
+  }
+  if (evt.code !== "Escape") {
+    return;
+  }
+  instance.close();
+}
+
+gallery.addEventListener("keydown", closeBigImg);
