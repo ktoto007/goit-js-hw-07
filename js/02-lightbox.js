608f347ac0929829fa34dec9e587094aa1752daf
Streltsov Valerii <st.valera04@gmail.com>
2023-02-03 00:57:07 +0200

    Done second exersise

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItemCollection = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a> `
+  )
+  .join("");
+
+gallery.innerHTML = galleryItemCollection;
+
+const captionsData = "alt";
+const captionDelay = 250;
+new SimpleLightbox(".gallery a", { captionsData, captionDelay });
